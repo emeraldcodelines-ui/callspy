9542cef00097f8bd5d203a924f6567428e600ed3
emeraldcodelines-ui <emeraldcodelines@gmail.com>
2025-12-01 18:46:16 +0100

    Update lib/main.dart to import HomeScreen and set home to HomeScreen with title CallSpy

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,18 @@
 import 'package:flutter/material.dart';
+import 'features/call_recording/presentation/screens/home_screen.dart';
 
 void main() {
-  runApp(const CallSpyApp());
+  runApp(const MyApp());
 }
 
-class CallSpyApp extends StatelessWidget {
-  const CallSpyApp({Key? key}) : super(key: key);
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'CallSpy',
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('CallSpy'),
-        ),
-        body: const Center(
-          child: Text('Welcome to CallSpy!'),
-        ),
-      ),
+      home: const HomeScreen(),
     );
   }
 }
